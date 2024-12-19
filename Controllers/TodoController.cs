a12b08a27dfd2748907b68228f1e12d159ec79f5
Afzal <wihappyx@gmail.com>
2024-12-19 18:39:08 +0500

    Added TodoController API with GET and POST methods
    - Created a new TodoController to handle API requests.
    - Implemented GET method to fetch all Todo items from the database.
    - Implemented POST method to create new Todo items and save them to the database.
    - Used async/await for asynchronous database operations with Entity Framework.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿using BlazorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TodoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodos()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> CreateTodo(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTodos), new { id = todoItem.Id }, todoItem);
+        }
+    }
+}
